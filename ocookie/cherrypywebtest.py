e0dee4fad47bff3939d2a34fbc6cb303f4bea0d5
Oleg Pudeyev <oleg@bsdpower.com>
2012-07-24 22:55:03 -0400

    cherrypywebtest import chase

--- ocookie/cherrypywebtest.py
+++ ocookie/cherrypywebtest.py
@@ -1,13 +1,13 @@
-from . import ocookie
+from . import CookieParser, CookieJar
 
 def parse_cookies(cpwt_cookies):
     '''Parses self.cookies after a getPage call.
     '''
     
-    cookies = [ocookie.CookieParser.parse_set_cookie_value(cookie_header[1]) for cookie_header in cpwt_cookies]
+    cookies = [CookieParser.parse_set_cookie_value(cookie_header[1]) for cookie_header in cpwt_cookies]
     return cookies
 
-class CpwtCookieJar(ocookie.CookieJar):
+class CpwtCookieJar(CookieJar):
     def update(self, cpwt_cookies):
         cookies = parse_cookies(cpwt_cookies)
         for cookie in cookies:
