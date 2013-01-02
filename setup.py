9c05031446d0d17bdc207b00ebf47d9769f96d33
Oleg Pudeyev <oleg@bsdpower.com>
2013-01-02 04:54:06 -0500

    Add a setup.py for owebunit to be able to obtain ocookie via pip

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ocookie',
+    version='0.1',
+    description='Comprehensive cookie library',
+    author='Oleg Pudeyev',
+    author_email='oleg@bsdpower.com',
+    url='http://github.com/p/ocookie',
+    packages=['ocookie'],
+)
