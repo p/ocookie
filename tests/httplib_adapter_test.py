ed98c6fc0c8872263a696d4d403eba773c759233
Oleg Pudeyev <oleg@bsdpower.com>
2013-01-14 06:50:27 -0500

    Deal with str/bytes mismatch in python 3

--- tests/httplib_adapter_test.py
+++ tests/httplib_adapter_test.py
@@ -6,8 +6,12 @@ from tests import app
 py3 = sys.version_info[0] == 3
 if py3:
     import http.client as httplib
+    def to_bytes(text):
+        return text.encode('utf8')
 else:
     import httplib
+    def to_bytes(text):
+        return text
 
 port = 5040
 app.run(port)
@@ -17,7 +21,7 @@ class HttplibAdapterTest(unittest.TestCase):
         conn = httplib.HTTPConnection('localhost', port)
         conn.request('GET', '/set')
         response = conn.getresponse()
-        self.assertEqual('success', response.read())
+        self.assertEqual(to_bytes('success'), response.read())
         
         cookies = ocookie.httplib_adapter.parse_response_cookies(response)
         self.assertEqual(1, len(cookies))
