4a441133e2b75b2a0de739e87b7b16c228290afa
Oleg Pudeyev <oleg@bsdpower.com>
2014-02-03 17:00:19 -0500

    Prepare for pypi

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from distutils.core import setup
 
 setup(name='ocookie',
-    version='0.1',
+    version='0.1.1',
     description='Comprehensive cookie library',
     author='Oleg Pudeyev',
     author_email='oleg@bsdpower.com',
     url='http://github.com/p/ocookie',
     packages=['ocookie'],
+    data_files=['LICENSE', 'README.rst'],
 )
